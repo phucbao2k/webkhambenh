142812c340b5120fa346629988462ee2efb64fb9
phucbao2k <91213246+phucbao2k@users.noreply.github.com>
2022-08-28 12:31:17 +0700

    l

---
+++ webcaytrongVNUA/Nodejs/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from './route/web';
+import _ from 'lodash';
+require('dotenv').config;
+let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+viewEngine(app);
+initWebRoutes(app);
+let port = process.env.PORT || 7070;
+//if port is undefined, default to current 7070
+app.listeners(port,()=>{
+    //callback
+console.log("port is" + port);
+})
