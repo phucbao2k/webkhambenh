f0cfb16118184e91cdcf059dd2eff2a060ec8228
phucbao2k <91213246+phucbao2k@users.noreply.github.com>
2022-08-28 15:46:39 +0700

    u

---
+++ webcaytrongVNUA/Nodejs/src/controllers/homeController.js
@@ -0,0 +1,10 @@
+let getHomePage =(req, res)=>{
+return res.render('homepage.ejs');
+}
+let getAboutPage =(req, res)=>{
+    return res.render('test/about.ejs');
+    }
+exports ={
+    getHomePage:getHomePage,
+    getAboutPage:getAboutPage,
+}
