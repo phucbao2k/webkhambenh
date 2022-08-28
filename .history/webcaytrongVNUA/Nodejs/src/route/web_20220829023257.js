400b26c646b7c3af9b77b2734014254242127a2e
phucbao2k <baophucta2k@gmail.com>
2022-08-29 02:36:30 +0700

    blast

--- .history/webcaytrongVNUA/Nodejs/src/route/web_20220829023257.js
+++ .history/webcaytrongVNUA/Nodejs/src/route/web_20220829023257.js
@@ -4,7 +4,7 @@ import homeController from "../controllers/homeController.js";
 let router = express.Router();
 export function initWebRoutes(app) {
     router.get('/', homeController.getHomePage);
-   // router.get('/', homeController.getAboutPage);
+    router.get('/', homeController.getAboutPage);
     router.get('/', (req, res) => {
         return res.send('HELU WORLD');
     });
