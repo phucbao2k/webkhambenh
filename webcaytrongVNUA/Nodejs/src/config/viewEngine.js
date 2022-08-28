d6fd39d84d39114265f5ba40254d6db3c5ae15dc
phucbao2k <91213246+phucbao2k@users.noreply.github.com>
2022-08-28 14:27:12 +0700

    u

--- webcaytrongVNUA/Nodejs/src/config/viewEngine.js
+++ webcaytrongVNUA/Nodejs/src/config/viewEngine.js
@@ -2,7 +2,7 @@ import express from "express";
 
 export function  configViewEngine(){
 app.use(express.static("./src/public"));
-app.set("view engine","ejs");
+app.set("view engine","ejs","cjs");
 app.set("views","./src/views");
 }
 module.exports = configViewEngine;
