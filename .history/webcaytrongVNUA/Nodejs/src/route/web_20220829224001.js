25dc7359ae56f68731fae679e8e8116494990a63
phucbao2k <baophucta2k@gmail.com>
2022-08-29 23:27:14 +0700

    g

---
+++ .history/webcaytrongVNUA/Nodejs/src/route/web_20220829224001.js
@@ -0,0 +1,12 @@
+import express from "express";
+import homeController from "../controllers/homeController.js";
+
+let router = express.Router();
+export function initWebRoutes(app) {
+    router.get('/', homeController.getHomePage);
+    router.get('/', homeController.getAboutPage);
+    router.get('/', (req, res) => {
+        return res.send('HELU WORLD');
+    });
+    return app.use("/", router);
+}
