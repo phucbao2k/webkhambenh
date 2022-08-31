4941564be54cb56076fa2ddb039c62364bc03d3b
phucbao2k <baophucta2k@gmail.com>
2022-08-31 10:25:57 +0700

    go

---
+++ .history/webcaytrongVNUA/Nodejs/src/migrations/migration-allcode_20220831102256.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('allcode', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      key: {
+        type: Sequelize.STRING
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+     address:{
+type: Sequelize.STRING
+     },
+     gender:{
+      type: Sequelize.BOOLEAN
+           },
+           roleid:{
+            type: Sequelize.STRING
+                 },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('allcode');
+  }
+};
---
+++ .history/webcaytrongVNUA/Nodejs/src/models/allcode_20220831102144.js
@@ -0,0 +1,24 @@
+'use strict';
+import { Model } from 'sequelize';
+export default (sequelize, DataTypes) => {
+  class Al extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({ 
+    key: DataTypes.STRING,
+    type: DataTypes.STRING,
+    value_en: DataTypes.STRING,
+    value_vi: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Allcode',
+  });
+  return Allcode;
+};
