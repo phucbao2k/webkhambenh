d98d2dad7803a1d264596a0bccddebcfd06c3d4f
phucbao2k <baophucta2k@gmail.com>
2022-09-02 00:19:38 +0700

    golang

---
+++ .history/webcaytrongVNUA/Nodejs/src/controllers/homeController_20220901234511.js
@@ -0,0 +1,38 @@
+import db from '../models/index';
+import CRUDService from '../services/CRUDService';
+let getHomePage = async (req, res) => { 
+    try{
+let data = await db.User.findAll();
+return res.render('homepage.ejs',{
+    data: JSON.stringify(data)
+});
+    }catch(e){
+        console.log(e);
+    }
+}
+
+let getAboutPage = (req, res) => {
+    return res.render('test/about.ejs');
+}
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+let postCRUD = async (req, res) => {
+ let message=  await CRUDService.createNewUser(req.body);
+ console.log(message);
+    return res.send('post');
+   
+}
+let displayGetCRUD = async (req, res) => { 
+    let data = await CRUDService.getAllUsers();
+    console.log('--------------------------------');
+    console.log(data);
+    console.log('--------------------------------');
+    return res.send('go go bros')}
+export default {
+    getHomePage: getHomePage,
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
+    displayGetCRUD: displayGetCRUD,   
+};
