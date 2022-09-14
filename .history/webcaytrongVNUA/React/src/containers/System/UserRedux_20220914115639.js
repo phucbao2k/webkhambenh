d5928e804334b973534abb01f613c2ca456d62e1
phucbao2k <baophucta2k@gmail.com>
2022-09-14 12:00:12 +0700

    f

--- .history/webcaytrongVNUA/React/src/containers/System/UserRedux_20220914115639.js
+++ .history/webcaytrongVNUA/React/src/containers/System/UserRedux_20220914115639.js
@@ -25,21 +25,21 @@ class UserRedux extends Component {
                         <form>
   <div className="form-row">
     <div className="col-md-4 mb-3">
-      <label htmlFor="validationServer01">First name</label>
+      <label for="validationServer01">First name</label>
       <input type="text" className="form-control is-invalid" id="validationServer01" placeholder="First name"  required/>
       <div className="invalid-feedback">
        Please enter a valid value
       </div>
     </div>
     <div className="col-md-4 mb-3">
-      <label htmlFor="validationServer02">Last name</label>
+      <label for="validationServer02">Last name</label>
       <input type="text" className="form-control is-invalid" id="validationServer02" placeholder="Last name"  required/>
       <div className="invalid-feedback">
        Please enter a valid value
       </div>
     </div>
     <div className="col-md-4 mb-3">
-      <label htmlFor="validationServerUsername">Username</label>
+      <label for="validationServerUsername">Username</label>
       <div className="input-group">
         <div className="input-group-prepend">
           <span className="input-group-text" id="inputGroupPrepend3">@</span>
@@ -53,21 +53,21 @@ class UserRedux extends Component {
   </div>
   <div className="form-row">
     <div className="col-md-6 mb-3">
-      <label htmlFor="validationServer03">City</label>
+      <label for="validationServer03">City</label>
       <input type="text" className="form-control is-invalid" id="validationServer03" placeholder="City" required/>
       <div className="invalid-feedback">
         Please provide a valid city.
       </div>
     </div>
     <div className="col-md-3 mb-3">
-      <label htmlFor="validationServer04">State</label>
+      <label for="validationServer04">State</label>
       <input type="text" className="form-control is-invalid" id="validationServer04" placeholder="State" required/>
       <div className="invalid-feedback">
         Please provide a valid state.
       </div>
     </div>
     <div className="col-md-3 mb-3">
-      <label htmlFor="validationServer05">Zip</label>
+      <label for="validationServer05">Zip</label>
       <input type="text" className="form-control is-invalid" id="validationServer05" placeholder="Zip" required/>
       <div className="invalid-feedback">
         Please provide a valid zip.
@@ -77,7 +77,7 @@ class UserRedux extends Component {
   <div className="form-group">
     <div className="form-check">
       <input className="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" required/>
-      <label className="form-check-label" htmlFor="invalidCheck3">
+      <label className="form-check-label" for="invalidCheck3">
         Agree to terms and conditions
       </label>
       <div className="invalid-feedback">
