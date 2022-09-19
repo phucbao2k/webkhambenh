dbc53a2bf50617e02c703f2bdd26bb7b70d826d7
phucbao2k <baophucta2k@gmail.com>
2022-09-19 19:41:10 +0700

    x

---
+++ .history/webcaytrongVNUA/React/src/services/userService_20220919192843.js
@@ -0,0 +1,36 @@
+import axios from '../axios';//import để gửi dữ liệu từ client lên server
+const handleLoginApi =(userEmail, userPassword)=>{
+    return axios.post('/api/login',{email: userEmail, password: userPassword});
+    //return mục đích để axios lấy thông tin ta muốn gửi từ phía client về server
+
+}
+const getAllUsers = (inputId)=>{
+    //Đây là Template string
+    // $ dùng để access hoặc truy cập vào 1 biến, và hiển thị ra
+    return axios.get(`/api/get-all-users?id=${inputId}`)
+}
+const createNewUserService = (data)=>{
+    console.log('data from server: ', data);
+    return axios.post('/api/create-new-user', data);
+}
+const deleteUserService =(userId)=>{
+    return axios.delete('/api/delete-user',{
+        data:{
+            id:userId
+        }
+    })
+}
+const editUserService =(inputData) =>{
+    return axios.put('/api/edit-user', inputData
+    );
+}
+const getAllCodeService = (inputType)=>{
+    return axios.get(`/api/allcode?type=${inputType}`)
+}
+const getTopDoctorHomeService =(limit)=>{
+    return axios.get(``)
+}
+export{handleLoginApi, getAllUsers,
+     createNewUserService,deleteUserService,
+     editUserService,getAllCodeService}//đối với ReactJs, khi export function để nơi khác truy cập, 
+//ta không cần export default hay module.export
---
+++ .history/webcaytrongVNUA/React/src/store/reducers/adminReducer_20220919194002.js
@@ -0,0 +1,108 @@
+import actionTypes from '../actions/actionTypes';
+
+const initialState = {
+   genders:[],
+   roleIds: [],
+   positions: [],
+   isLoadingGenders: false,
+   users: []
+}
+
+const adminReducer = (state = initialState, action) => {
+    switch (action.type) {
+        // case actionTypes.ADMIN_LOGIN_SUCCESS:
+        //     return {
+        //         ...state,
+        //         isLoggedIn: true,
+        //         adminInfo: action.adminInfo
+        //     }
+        // case actionTypes.ADMIN_LOGIN_FAIL:
+        //     return {
+        //         ...state,
+        //         isLoggedIn: false,
+        //         adminInfo: null
+        //     }
+        // case actionTypes.PROCESS_LOGOUT:
+        //     return {
+        //         ...state,
+        //         isLoggedIn: false,
+        //         adminInfo: null
+        //     }
+        case actionTypes.FETCH_GENDER_START:
+            let copy1 = {...state};
+            copy1.isLoadingGenders = true;
+            return{
+              ...copy1
+
+                
+            }
+            case actionTypes.FETCH_GENDER_SUCCESS:
+               state.genders = action.data;
+               state.isLoadingGenders= false;
+                return{
+                    ...state,
+                }
+                case actionTypes.FETCH_GENDER_FAILED:
+                    state.isLoadingGenders= false;
+                    state.genders=[];
+                    return{
+                        ...state,
+                    }
+                    case actionTypes.FETCH_POSITION_START:
+                        let copy2 = {...state};
+            copy2.isLoadingGenders = true;
+            return{
+                ...copy2,
+            }
+            case actionTypes.FETCH_POSITION_SUCCESS:
+                state.positions = action.data;
+                return{
+                    ...state,
+                }
+                case actionTypes.FETCH_POSITION_FAILED:
+                    state.positions =[];
+                    return{
+                        ...state,
+                    }
+                    case actionTypes.FETCH_ROLE_START:
+                        let copy3 = {...state};
+            copy3.isLoadingGenders = true;
+                        return{
+                            ...copy3,
+                        }
+                    case actionTypes.FETCH_ROLE_SUCCESS:
+                state.roleIds = action.data;
+                return{
+                    ...state,
+                }
+                case actionTypes.FETCH_ROLE_FAILED:
+                    state.roleIds=[];
+                    return{
+                        ...state,
+                    }
+                    case actionTypes.FETCH_ALL_USER_SUCCESS:
+                        state.users = action.users;
+                        return{
+                            ...state,
+                        }
+                        case actionTypes.FETCH_ALL_USER_FAILED:
+                            state.users = [];
+                            return{
+                                ...state,
+                            }
+                            case actionTypes.FETCH_TOP_DOCTORS_SUCCESS:
+                                state.topD = action.users;
+                                return{
+                                    ...state,
+                                }
+                                case actionTypes.FETCH_ALL_USER_FAILED:
+                                    state.users = [];
+                                    return{
+                                        ...state,
+                                    }
+        default:
+            return state;
+    }
+}
+
+export default adminReducer;
