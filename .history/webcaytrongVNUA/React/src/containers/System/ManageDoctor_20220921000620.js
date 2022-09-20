cf8aba237a21ca21b68b36c6995153f9413f8354
phucbao2k <baophucta2k@gmail.com>
2022-09-21 00:07:23 +0700

    d

--- .history/webcaytrongVNUA/React/src/containers/System/ManageDoctor_20220921000620.js
+++ .history/webcaytrongVNUA/React/src/containers/System/ManageDoctor_20220921000620.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
-import './ManageDoctor.scss';
+// import './ManageDoctor.scss';
 import * as actions from "../../store/actions";
 import MarkdownIt from 'markdown-it';
 import MdEditor from 'react-markdown-editor-lite';
