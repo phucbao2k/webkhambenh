7503f12cf8236d8e339c97c701cbf027ac4ae221
phucbao2k <baophucta2k@gmail.com>
2022-09-20 17:52:13 +0700

    d

---
+++ .history/webcaytrongVNUA/Nodejs/src/migrations/migration-markdown_20220920174846.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('markdowns', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      contentHTML: {
+        allowNull: false,
+        type: Sequelize.TEXT('long')
+      },
+      contentMarkdown: {
+        allowNull: false,
+        type: Sequelize.TEXT('long')
+      },
+      des: {
+        type: Sequelize.STRING
+      },
+     valueVi:{
+type: Sequelize.STRING
+     },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('allcodes');
+  }
+};
