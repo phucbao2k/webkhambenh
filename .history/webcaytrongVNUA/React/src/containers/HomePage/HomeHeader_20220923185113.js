af0ff5d520197ea253b9ac001d9044ea147ea07e
phucbao2k <baophucta2k@gmail.com>
2022-09-23 18:52:25 +0700

    e

--- .history/webcaytrongVNUA/React/src/containers/HomePage/HomeHeader_20220923185113.js
+++ .history/webcaytrongVNUA/React/src/containers/HomePage/HomeHeader_20220923185113.js
@@ -56,45 +56,7 @@ class HomeHeader extends Component {
                 </div>
                
             </div>
-            {this.props.isShowBanner === true && 
-                <div className="home-header-banner">
-                <div className="content-up">
-                <div className="title1"><FormattedMessage id="banner.title1"/></div>
-                <div className="title2"><FormattedMessage id="banner.title2"/></div>
-                <div className="search"><i className="fa-solid fa-magnifying-glass"></i>
-                <input type="text" placeholder='Search...'></input></div>
-                </div>
-               <div className="content-down">
-               <div className="options">
-                <div className="option-child">
-                    <div className="icon-child"><i className="fa-solid fa-tree"></i></div>
-                    <div className="text-child"><FormattedMessage id="banner.child1"/></div>
-                </div>
-                <div className="option-child">
-                    <div className="icon-child"><i className="fa-solid fa-mobile-screen-button"></i></div>
-                    <div className="text-child"><FormattedMessage id="banner.child2"/></div>
-                </div>
-                <div className="option-child">
-                    <div className="icon-child"><i className="fa-solid fa-book-medical"></i></div>
-                    <div className="text-child"><FormattedMessage id="banner.child3"/></div>
-                </div>
-                <div className="option-child">
-                    <div className="icon-child"><i className="fa-solid fa-mosquito"></i></div>
-                    <div className="text-child"><FormattedMessage id="banner.child4"/></div>
-                </div>
-                <div className="option-child">
-                    <div className="icon-child"><i className="fa-brands fa-pagelines"></i></div>
-                    <div className="text-child"><FormattedMessage id="banner.child5"/></div>
-                </div>
-                <div className="option-child">
-                    <div className="icon-child"><i className="fa-solid fa-sun-plant-wilt"></i></div>
-                    <div className="text-child"><FormattedMessage id="banner.child6"/></div>
-                </div>
-               </div>
-               </div>
- 
-            </div>
-               }
+            
             </React.Fragment>
           
         )
