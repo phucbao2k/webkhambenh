8b0829c4ff955b807f7fd9fac7057d068d00d843
phucbao2k <baophucta2k@gmail.com>
2022-10-10 01:28:39 +0700

    j

---
+++ .history/webcaytrongVNUA/React/src/containers/Patient/Doctor/ProfileDoctor_20221010010535.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+import './ProfileDoctor.scss';
+import {getProfileDoctorById} from '../../../services/userService';
+import {LANGUAGES} from '../../../
+//lodash hỗ trợ ta kiểm tra và thao tác với mảng dễ dàng hơn
+
+class ProfileDoctor extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+
+    }
+
+
+    async componentDidMount() {
+
+    }
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.language !== prevProps.language) {
+
+        }
+    }
+
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileDoctor);
+
+
+
+
---
+++ .history/webcaytrongVNUA/React/src/containers/Patient/Doctor/ProfileDoctor_20221010012010.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+import './ProfileDoctor.scss';
+import { getProfileDoctorById } from '../../../services/userService';
+import { LANGUAGES } from '../../../utils';
+import NumberFormat from 'react-number-format';
+//lodash hỗ trợ ta kiểm tra và thao tác với mảng dễ dàng hơn
+
+class ProfileDoctor extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataProfile: {}
+        }
+
+    }
+
+
+    async componentDidMount() {
+        let data = await this.getInforDoctor(this.props.doctorId);
+        this.setState({ dataProfile: data });
+    }
+    getInforDoctor = async (id) => {
+        let result = {};
+        if (id) {
+            let res = await getProfileDoctorById(id);
+            if (res && res.errCode === 0) {
+                result = res.data;
+            }
+        }
+        return result;
+    }
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.language !== prevProps.language) {
+
+        }
+        if (this.props.doctorId !== prevProps.doctorId) {
+
+        }
+    }
+
+
+    render() {
+        let { dataProfile } = this.state;
+        let { language } = this.props;
+        let nameVi = '', nameEn = '';
+        if (dataProfile && dataProfile.positionData) {
+            nameVi = `${dataProfile.positionData.valueVi}, ${dataProfile.firstName} ${dataProfile.lastName}`;
+            nameEn = `${dataProfile.positionData.valueVi}, ${dataProfile.lastName} ${dataProfile.firstName}`;
+        }
+        return (
+            <div className="profile-doctor-container">
+<div className="intro-doctor">
+    <div className="content-left"
+    style={{backgroundImage: `url(${dataProfile && dataProfile.image? dataProfile: ''})`}}>
+        
+    </div>
+    <div className="content-right">
+        <div className="up">
+            {language === LANGUAGES.VI ? nameVi: nameEn}
+        </div>
+        
+    </div>
+</div>
+            </div>
+        )
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileDoctor);
+
+
+
+
