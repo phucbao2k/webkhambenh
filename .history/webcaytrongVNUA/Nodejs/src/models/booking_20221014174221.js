b2a80c67e0cf9ac0a617bf841be23dea8c8f1565
phucbao2k <baophucta2k@gmail.com>
2022-10-14 17:48:01 +0700

    d

---
+++ .history/webcaytrongVNUA/Nodejs/src/models/booking_20221014174221.js
@@ -0,0 +1,28 @@
+'use strict';
+import { Model } from 'sequelize';
+export default (sequelize, DataTypes) => {
+  class Booking extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Booking.init({
+    image: DataTypes.STRING,
+    statusId: DataTypes.STRING,
+    doctorId: DataTypes.INTEGER,
+    patientId: DataTypes.INTEGER,
+    plantName: DataTypes.STRING,
+    specialtyName: DataTypes.STRING,
+    timeType: DataTypes.STRING,
+    date
+  }, {
+    sequelize,
+    modelName: 'Booking',
+  });
+  return Booking;
+};
