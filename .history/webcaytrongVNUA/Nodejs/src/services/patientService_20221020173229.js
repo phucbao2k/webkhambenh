536046673a0de2c425ddb067e35fa320e844fc2f
phucbao2k <baophucta2k@gmail.com>
2022-10-20 17:48:49 +0700

    j

---
+++ .history/webcaytrongVNUA/Nodejs/src/services/patientService_20221020173229.js
@@ -0,0 +1,60 @@
+import db from "../models/index";
+require('dotenv').config();
+import emailService from './emailService';
+import {v4 as}
+let postBookAppointment = (data) => {
+    return new Promise(async(resolve, reject) =>{
+try{
+    if (!data.email || !data.doctorId || !data.timeType || !data.specialtyName  || !data.fullName
+        || !data.date
+    ){
+    resolve({
+        errCode: 1,
+        errMessage: 'Missing required fields'
+    })
+}else{
+    await emailService.sendSimpleEmail({
+        receiverEmail: data.email,
+        patientName: data.fullName,
+        time: data.timeString,
+        doctorName:data.doctorName,
+        diagnosis:data.diagnosis,
+        language: data.language,
+        redirectLink: 'https://www.facebook.com/ta.phucbao'
+    })
+    let user = await db.User.findOrCreate({
+        where: {email: data.email},
+        defaults:{
+            email: data.email,
+            roleId: 'R3'
+        },
+    });
+    
+    console.log('check customer: ', user[0])
+    if(user && user[0]){
+        await db.Booking.create({        
+                statusId: 'S1',
+                doctorId: data.doctorId,
+                patientId: user[0].id,
+                specialtyName: data.specialtyName,
+                plantName: data.plantName,
+                timeType: data.timeType,
+                image: data.image,
+                date: data.date,
+                token: data.token,
+
+        })
+    }
+    resolve({
+        errCode: 0,
+        errMessage: 'Save infor patient succeed!'
+    })
+}
+}catch(e){
+reject(e);
+}
+    })
+}
+module.exports ={
+    postBookAppointment: postBookAppointment
+}
