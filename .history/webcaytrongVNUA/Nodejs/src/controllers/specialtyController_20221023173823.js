e12593bbaf4bca4fb96a9c583fde6323a8f48e45
phucbao2k <baophucta2k@gmail.com>
2022-10-23 17:38:35 +0700

    d

--- .history/webcaytrongVNUA/Nodejs/src/controllers/specialtyController_20221023173823.js
+++ .history/webcaytrongVNUA/Nodejs/src/controllers/specialtyController_20221023173823.js
@@ -1,9 +1,9 @@
 import specialtyService from "../services/specialtyService";
-let createSpecialty = async (req, res) => {
-    try {
+let createSpecialty = async(req, res) => {
+    try{
         let infor = await specialtyService.createSpecialty(req.body);
-        return res.status(200).json(infor);
-    } catch (e) {
+return res.status(200).json(infor);
+    }catch(e){
         console.log(e);
         return res.status(200).json({
             errCode: -1,
@@ -12,6 +12,6 @@ let createSpecialty = async (req, res) => {
 
     }
 }
-module.exports = {
+module.exports ={
     createSpecialty: createSpecialty
 }
