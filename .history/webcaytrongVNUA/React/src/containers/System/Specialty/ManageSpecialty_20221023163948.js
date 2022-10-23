83afa5508d650939085499c20efe837a13cf3c1f
phucbao2k <baophucta2k@gmail.com>
2022-10-23 17:10:07 +0700

    f

---
+++ .history/webcaytrongVNUA/React/src/containers/System/Specialty/ManageSpecialty_20221023163948.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+import './ManageSpecialty.scss';
+import MarkdownIt from 'markdown-it';
+import MdEditor from 'react-markdown-editor-lite';
+import {CommonUtils} from '../../../utils';
+import {createNewSpecialty} from ''
+
+//lodash hỗ trợ ta kiểm tra và thao tác với mảng dễ dàng hơn
+
+class ManageSpecialty extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+
+    }
+
+
+    async componentDidMount() {
+
+    }
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.language !== prevProps.language) {
+
+        }
+    }
+
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageSpecialty);
+
+
+
+
---
+++ .history/webcaytrongVNUA/React/src/containers/System/Specialty/ManageSpecialty_20221023164521.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+import './ManageSpecialty.scss';
+import MarkdownIt from 'markdown-it';
+import MdEditor from 'react-markdown-editor-lite';
+import {CommonUtils} from '../../../utils';
+import {createNewSpecialty} from '../../../services/userService';
+import {toast} from "react-toastify";
+
+//lodash hỗ trợ ta kiểm tra và thao tác với mảng dễ dàng hơn
+const mdParser = new MarkdownIt();
+
+class ManageSpecialty extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            imageBase64: '',
+            descriptionHTML: '',
+            descriptionMarkdown: '',
+
+        }
+
+    }
+
+
+    async componentDidMount() {
+
+    }
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.language !== prevProps.language) {
+
+        }
+    }
+handleOnChangeInput = (event, id) => {
+    let stateCopy ={...this.state}
+    stateCopy[id] = event.target.value;
+    this.setState({
+        ...stateCopy
+    })
+}
+    handleEditorChange = ({ html, text }) => {
+        this.setState({
+            descriptionMarkdown: text,
+            descriptionHTML: html,
+        })
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageSpecialty);
+
+
+
+
