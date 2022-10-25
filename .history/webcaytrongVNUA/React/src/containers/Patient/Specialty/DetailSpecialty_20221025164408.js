0e95e0f3dcaedf23eb48f1c7cb3ee6ea5835195e
phucbao2k <baophucta2k@gmail.com>
2022-10-25 17:12:21 +0700

    d

---
+++ .history/webcaytrongVNUA/React/src/containers/Patient/Specialty/DetailSpecialty_20221025164408.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+import './DetailSpecialty.scss';
+import HomeHeader from '../../HomePage/HomeHeader';
+import DoctorSchedule from '../Doctor/DoctorSchedule';
+import DoctorExtraInfor from '../Doctor/DoctorExtraInfor';
+import ProfileDoctor from '../Doctor/ProfileDoctor';
+//lodash hỗ trợ ta kiểm tra và thao tác với mảng dễ dàng hơn
+
+class DetailSpecialty extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrDoctorId: [44, 45, 46]
+
+        }
+
+    }
+
+
+    async componentDidMount() {
+
+    }
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.language !== prevProps.language) {
+
+        }
+    }
+
+
+    render() {
+        return (
+            <>
+            <HomeHeader/>
+            <div>HELLU </div>
+            </>
+        )
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DetailSpecialty);
+
+
+
+
