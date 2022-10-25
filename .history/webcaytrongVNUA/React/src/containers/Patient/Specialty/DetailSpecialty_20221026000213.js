94c8bfb028eeeb98ca4f19df515c7b1eacb6a6be
phucbao2k <baophucta2k@gmail.com>
2022-10-26 00:29:37 +0700

    f

---
+++ .history/webcaytrongVNUA/React/src/containers/Patient/Specialty/DetailSpecialty_20221026000213.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+import './DetailSpecialty.scss';
+import HomeHeader from '../../HomePage/HomeHeader';
+import DoctorSchedule from '../Doctor/DoctorSchedule';
+import DoctorExtraInfor from '../Doctor/DoctorExtraInfor';
+import ProfileDoctor from '../Doctor/ProfileDoctor';
+//lodash hỗ trợ ta kiểm tra và thao tác với mảng dễ dàng hơn
+import {getAllCodeService, getAllDetailSpecialtyById} from '../../../services/userService'
+class DetailSpecialty extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrDoctorId: [2, 3]
+
+        }
+
+    }
+
+
+    async componentDidMount() {
+
+    }
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.language !== prevProps.language) {
+
+        }
+    }
+
+
+    render() {
+        let { arrDoctorId } = this.state;
+        return (
+            <div className="detail-specialty-container">
+                <HomeHeader />
+                <div className="detail-specialty-body">
+                    <div className="description-specialty">
+                    </div>
+                    {arrDoctorId && arrDoctorId.length>0 &&
+                    arrDoctorId.map((item, index)=> {
+                        return(
+                            <div className="each-doctor" key={index}>
+                                <div className="dt-content-left">
+                                    <div className="profile-doctor">
+                                        <ProfileDoctor
+                                        doctorId={item}
+                                        isShowDescriptionDoctor={true}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="dt-content-right">
+                                    <div className="doctor-schedule">
+                                        <DoctorSchedule
+                                        doctorIdFromParent={item}
+                                        />
+                                    </div>
+                                    <div className="doctor-extra-infor">
+                                        <DoctorExtraInfor
+                                        doctorIdFromParent={item}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        )
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DetailSpecialty);
+
+
+
+
---
+++ .history/webcaytrongVNUA/React/src/containers/Patient/Specialty/DetailSpecialty_20221026001956.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+import './DetailSpecialty.scss';
+import HomeHeader from '../../HomePage/HomeHeader';
+import DoctorSchedule from '../Doctor/DoctorSchedule';
+import DoctorExtraInfor from '../Doctor/DoctorExtraInfor';
+import ProfileDoctor from '../Doctor/ProfileDoctor';
+//lodash hỗ trợ ta kiểm tra và thao tác với mảng dễ dàng hơn
+import {getAllCodeService, getAllDetailSpecialtyById} from '../../../services/userService';
+import _ from 'lodash';
+import {LANGUAGES} from '../../../utils';
+class DetailSpecialty extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrDoctorId: [],
+            dataDetailSpecialtyId: {},
+            listProvince: []
+
+        }
+
+    }
+
+
+    async componentDidMount() {
+        if(this.props.match && this.props.match.params && this.props.match.params.id){
+            let id = this.props.match.params.id;
+            let res = await getAllDetailSpecialtyById({
+                id: id,
+                location: 'ALL'
+            });
+            let resProvince = await getAllCodeServices('PROVINCE');
+            if(res && res.errCode === 0 && resProvince && resProvince.errCode === 0){
+                let data = res.data;
+                let arrDoctorId = [];
+                if(data && !_.isEmpty(data)){
+                    let arr = data.doctorSpecialty;
+                    if(arr && arr.length > 0){
+                        arr.map(item => {
+                            arrDoctorId.push(item.doctorId)
+                        })
+                    }
+                }
+                this.setState({
+                    dataDetailSpecialtyId: res.data,
+                    arrDoctorId: arrDoctorId,
+                    listProvince: resProvince.data
+                })
+            }
+        }
+
+    }
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.language !== prevProps.language) {
+
+        }
+    }
+handleOnChangeSelect = (event)=> {
+    console.log('check onchange', event.target.value)
+}
+
+    render() {
+        let { arrDoctorId, dataDetailSpecialty, listProvince } = this.state;
+        console.log('chuyen nganh check state', this.state)
+        let {language} = this.props;
+        return (
+            <div className="detail-specialty-container">
+                <HomeHeader />
+                <div className="detail-specialty-body">
+                    <div className="description-specialty">
+                        {dataDetailSpecialty && !_.isEmpty(dataDetailSpecialty)
+                        &&<div dangerouslySetInnerHTML={{ __html: dataDetailSpecialty.descriptionHTML}}>
+                            </div>
+                            }
+                    </div>
+                    <div className="search-sp-doctor">
+                        <Select
+                    </div>
+                    {arrDoctorId && arrDoctorId.length>0 &&
+                    arrDoctorId.map((item, index)=> {
+                        return(
+                            <div className="each-doctor" key={index}>
+                                <div className="dt-content-left">
+                                    <div className="profile-doctor">
+                                        <ProfileDoctor
+                                        doctorId={item}
+                                        isShowDescriptionDoctor={true}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="dt-content-right">
+                                    <div className="doctor-schedule">
+                                        <DoctorSchedule
+                                        doctorIdFromParent={item}
+                                        />
+                                    </div>
+                                    <div className="doctor-extra-infor">
+                                        <DoctorExtraInfor
+                                        doctorIdFromParent={item}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        )
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DetailSpecialty);
+
+
+
+
