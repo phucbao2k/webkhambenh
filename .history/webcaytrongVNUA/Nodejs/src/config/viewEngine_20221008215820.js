94dcc4724d467d05a739fb73072b0dfa5edcd291
phucbao2k <baophucta2k@gmail.com>
2023-02-06 00:27:17 +0700

    g

--- .history/webcaytrongVNUA/Nodejs/src/config/viewEngine_20221008215820.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-export function configViewEngine(app) {
-    app.use(express.static("./src/public"));
-    app.set("view engine", "ejs", "cjs");
-    app.set("views", "./src/views");
-}
-//
